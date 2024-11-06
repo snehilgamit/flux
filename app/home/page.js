e0f6fff273d2a3ecf15558cf8d492771b61615f2
snehil <108514128+snehilgamit@users.noreply.github.com>
2024-11-06 13:16:02 +0530

    Update page.js

--- app/home/page.js
+++ app/home/page.js
@@ -88,7 +88,7 @@ const home = () => {
     }, [])
     return (
         <>{isLogined ?
-            <div className='overflow-hidden'>
+            <>
                 <div className='w-full bg-black overflow-hidden relative h-[calc(100%-94px)] animate__animated animate__fadeIn'>
                     <div className='overflow-hidden no-scrollbar w-full overflow-y-scroll'>
                         {components[currentTab.current].header && <Header title={components[currentTab.current].title} />}
@@ -97,7 +97,7 @@ const home = () => {
                     </div>
                 </div>
                 <Menubar changeTab={changeTab} />
-            </div>
+            </>
             :
             <LoadingPage />
         }
