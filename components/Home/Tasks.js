53090ca6ed18550bb58de3235a8877d1b4d3a2cd
snehil <108514128+snehilgamit@users.noreply.github.com>
2024-11-06 13:14:13 +0530

    Update Tasks.js

--- components/Home/Tasks.js
+++ components/Home/Tasks.js
@@ -95,7 +95,7 @@ const Tasks = ({ user, fetchUser, tasks }) => {
   }
 
   return (
-    <div className='w-full pb-28'>
+    <div className='w-full'>
       <Toaster />
       <div className='px-4'>
         {events.map((element, index) => (
